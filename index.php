d7c2b61fac6c3b13baf29bc58161f6f448b6a736
Mahnoor Fatima <aminadreamy568@gmail.com>
2025-09-17 10:48:50 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// This is a PHP comment
+
+// Output text
+echo "Hello, World!";
+
+// Variables
+$name = "Amina";
+$age = 22;
+
+echo "<br>My name is $name and I am $age years old.";
+
+// Simple if condition
+if ($age >= 18) {
+    echo "<br>You are an adult.";
+} else {
+    echo "<br>You are a minor.";
+}
+?>
